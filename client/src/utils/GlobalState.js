60b0fea0013ecdf981045a287bf2cf86950df443
Vincent T <vyncode@gmail.com>
2021-08-19 21:45:46 -0400

    redux store made

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,8 +1,4 @@
-import React, { createContext, useContext } from "react";
-import { useProductReducer } from './reducers'
 
-const StoreContext = createContext();
-const { Provider } = StoreContext;
 
 const StoreProvider = ({ value = [], ...props }) => {
   const [state, dispatch] = useProductReducer({
---
+++ client/src/utils/store.js
@@ -0,0 +1,10 @@
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+export default createStore(reducers, {
+    products: [],
+    cart: [],
+    cartOpen: false,
+    categories: [],
+    currentCategory: '',
+});
