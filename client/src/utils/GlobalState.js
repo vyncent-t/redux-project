4141fa723da9024078ff46fcf371bb5195ea0928
Vincent T <vyncode@gmail.com>
2021-08-20 23:32:57 -0400

    fixed react error

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,6 +1,6 @@
 // delete old context code
 import React from "react";
-import { Provider } from "react";
+import { Provider } from "react-redux";
 import store from "./store";
 
 const StoreProvider = ({ ...props }) => {
